7baf13c7086b42d99ce9fa135393b07ceedc331f
notfluxW <blackvalentine523@gmail.com>
2023-08-15 18:19:40 +0200

    Added SQL Individuality

--- src/main.py
+++ src/main.py
@@ -13,21 +13,44 @@ timestamped_folder = os.path.join(output_folder, timestamp)
 lua_file_path = os.path.join(timestamped_folder, "generated.lua")
 sql_file_path = os.path.join(timestamped_folder, "generated.sql")
 
-def generate_lua_file(image_names):
-    print("\n\n>>> Lua Generation <<<\n")
-    isFood = input("Food or Liquid (F / L): ")
-    if isFood != "F" and isFood != "L":
-        isFood = input("Food or Liquid (F / L): ")
-    isFood = (isFood == "F")
-    percentage = float(input("Enter amount it should fill up (%): "))
+def isAnswerValid(answer, answers, doPrint):
+    for v in answers:
+        if v == answer:
+            return True
+    if doPrint: print("Error! Invalid answer.")
+    return False
+
+def PythonArrayToLua(array):
     lua_array = "{"
     index = 0
-    for item in image_names:
+    for item in array:
         index += 1
-        if index < len(image_names):
+        if index < len(array):
             lua_array = lua_array + "'" + item + "'" + ', '
         else:
             lua_array = lua_array + "'" + item + "'" + '}'
+    return lua_array
+
+def checkArray(array, value):
+    for v in array:
+        if v == value:
+            return True
+    return False
+
+def generate_lua_file(image_names):
+    print("\n\n>>> Lua Generation <<<\n")
+    isFood = input("Food or Liquid (F / L): ")
+    while not isAnswerValid(isFood.upper(), ["F", "L"], True):
+        isFood = input("Food or Liquid (F / L): ")
+    isFood = (isFood.upper() == "F")
+    while True:
+        percentage = input("Enter amount it should fill up (%): ")
+        try: 
+            percentage = float(percentage)
+            break
+        except ValueError:
+            print("Error! Invalid answer.")
+    lua_array = PythonArrayToLua(image_names)
 
     if isFood:
         lua_code = f"""for _,item in pairs({lua_array}) do
@@ -57,7 +80,7 @@ end"""
     with open(lua_file_path, "w") as lua_file:
         lua_file.write(lua_code)
     Choice = input("Would you like to generate SQL too? (Y / N): ")
-    while Choice != "Y" and Choice != "N":
+    while not isAnswerValid(Choice, ["Y", "y", "N", "n"], True):
         Choice = input("Can remove (Y / N): ")
     if Choice == "Y":
         generate_sql_file(image_names)
@@ -65,19 +88,61 @@ end"""
 
 def generate_sql_file(python_array):
     print("\n\n>>> SQL Generation <<<\n")
-    Label = str(input("Give your items a global label: "))
-    Limit = int(input("Give your items a global limit: "))
-    Can_Remove = input("Can remove (Y / N): ")
-    while Can_Remove != "Y" and Can_Remove != "N":
-        Can_Remove = input("Can remove (Y / N): ")
-    if Can_Remove == "Y":
-        Can_Remove = 1
+    Individually = str(input("Would you like to name your items individually? (Y / N): "))
+    while not isAnswerValid(Individually, ["Y", "y", "N", "n"], True):
+        Individually = str(input("Would you like to setup your items individually? (Y / N): "))
+    Individually = (Individually.upper() == "Y")
+    if Individually: 
+        Label = str(input("Give your items a standard label: "))
+        Limit = int(input("Give your items a standard limit: "))
+        Can_Remove = input("Give your items a standard can remove (Y / N): ")
+        while not isAnswerValid(Can_Remove, ["Y", "y", "N", "n"], True):
+            Can_Remove = input("Can remove (Y / N): ")
+        if Can_Remove == "Y":
+            Can_Remove = 1
+        else:
+            Can_Remove = 0
     else:
-        Can_Remove = 0
+        Label = str(input("Give your items a global label: "))
+        Limit = int(input("Give your items a global limit: "))
+        Can_Remove = input("Give your items a global can remove (Y / N): ")
+        while not isAnswerValid(Can_Remove, ["Y", "y", "N", "n"], True):
+            Can_Remove = input("Can remove (Y / N): ")
+        if Can_Remove == "Y":
+            Can_Remove = 1
+        else:
+            Can_Remove = 0
     
     sql_code = "INSERT INTO `items` (`name`, `label`, `limit`, `rare`, `can_remove`) VALUES"
-    for item in python_array:
-        sql_code = sql_code + f"\n    ('{item}', '{Label}', {Limit}, 0, {Can_Remove}),"
+    if Individually:
+        print(">>> Item Settings (Press Enter to use Standard Value) <<<")
+        for item in python_array:
+            print(f"\nItem Name: {item}")
+
+            itemLabel = input("Item Label: ")
+            if not itemLabel or itemLabel == "": itemLabel = Label
+
+            itemLimit = input("Item Limit: ")
+            if not itemLimit or itemLimit == "": itemLimit = Limit
+            else:
+                itemLimit = int(itemLimit)
+
+            itemCanRemove = str(input("Can Remove (Y / N): "))
+            while not isAnswerValid(itemCanRemove, ["Y", "y", "N", "n", ""], True):
+                itemCanRemove = str(input("Can Remove (Y / N): "))
+            if not itemCanRemove or itemCanRemove == "":
+                itemCanRemove = Can_Remove
+            else:
+                if itemCanRemove.upper() == "Y":
+                    itemCanRemove = 1
+                else:
+                    itemCanRemove = 0
+
+            sql_code = sql_code + f"\n    ('{item}', '{itemLabel}', {itemLimit}, 0, {itemCanRemove}),"
+            print(" ")
+    else:
+        for item in python_array:
+            sql_code = sql_code + f"\n    ('{item}', '{Label}', {Limit}, 0, {Can_Remove}),"
     with open(sql_file_path, "w") as sql_file:
         sql_file.write(sql_code)
     print("SQL file generated successfully!")
@@ -116,14 +181,12 @@ def main():
     generate_lua_file(image_names)
     print("\n\n>>> Additional Question <<<\n")
     response = input("Would you like to copy your images to another directory? (Y / N): ")
-    while response != "Y" and response != "N":
+    while not isAnswerValid(response, ["Y", "y", "N", "n"], True):
         response = input("Would you like to copy your images to another directory? (Y / N): ")
-    if response == "Y":
-        response = "yes"
-    else:
-        response = "no"
+    if response.upper() == "Y" : response = True 
+    else: response = False
 
-    if response == "yes":
+    if response:
         image_files = []
 
         for filename in os.listdir(directory):
